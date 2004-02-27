a46e1a29cb93fe7a527814b49a002523c68744ac
asouzis <devnull@localhost>
2004-02-27 20:04:03 +0000

    changes for release 0.2.0

--- rx4rdf/site/site-config.py
+++ rx4rdf/site/site-config.py
@@ -3,12 +3,12 @@ BASE_MODEL_URI='http://rx4rdf.sf.net/site/'
 __include__('../rx/rhizome/rhizome-config.py')
 
 #override default template by adding or replacing pages
-templateList =  rhizome.addItemTuple('RxPathSpec',loc='path:rxpathspec.txt', format='rhizml', doctype='specification')\
-+ rhizome.addItemTuple('faq',loc='path:faq.txt', format='rhizml', doctype='faq')\
-+ rhizome.addItemTuple('DocSample',loc='path:docsample.txt', format='rhizml', doctype='document')
-#+ rhizome.addItemTuple('Todo',loc='path:todo.txt', format='rhizml', doctype='todo')
+__addItem__('RxPathSpec',loc='path:rxpathspec.txt', format='rhizml', doctype='specification')
+__addItem__('faq',loc='path:faq.txt', format='rhizml', doctype='faq')
+__addItem__('DocSample',loc='path:docsample.txt', format='rhizml', doctype='document')
+#__addItem__('Todo',loc='path:todo.txt', format='rhizml', doctype='todo')
 
-siteVars =\
+__addSiteVars__(
 '''
  base:site-template:
   wiki:header-image: `Rx4RDFlogo.gif
@@ -19,8 +19,6 @@ siteVars =\
 
  base:sidebar:
    auth:needs-token: base:write-structure-token 
-''' 
-templateList.append( ('@sitevars', rxml.rhizml2nt(contents=siteVars, nsMap=nsMap)) )
+''')
 
-templateMap.update( dict(templateList))
-STORAGE_TEMPLATE = "".join(templateMap.values())
+
