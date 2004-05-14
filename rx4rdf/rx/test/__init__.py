9cf81751efd889f5df2335cdb29f150616bea28a
asouzis <devnull@localhost>
2004-05-14 01:46:04 +0000

    make a package so setup doesn't complain

---
+++ rx4rdf/rx/test/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Copyright (c) 2003 by Adam Souzis <asouzis@users.sf.net>
+    All rights reserved, see COPYING for details.
+    http://rx4rdf.sf.net    
+"""
+__all__ = ['glockTest.py', 'raccoonTest.py', 'RDFDomTest.py',
+           'testmodpatching.py', 'utilsTest.py', 'zmlTest.py']
